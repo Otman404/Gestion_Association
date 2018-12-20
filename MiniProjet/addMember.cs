7b5bf9baa7d172b4143585bd8e76a8284ab17dce
Otman404 <outhmaneboughaba98@gmail.com>
2018-12-19 18:31:00 -0800

    still working

--- MiniProjet/addMember.cs
+++ MiniProjet/addMember.cs
@@ -79,50 +79,50 @@ namespace MiniProjet
             string statut = statutCBX.Text;
             string statut_civil = statutCivilCBX.Text;
             string position = positionCBX.Text;
-            decimal somme_arg = decimal.Parse(argentTB.Text);
+            decimal somme_arg = 0;
+            if (argentTB.Text != "")
+            somme_arg = decimal.Parse(argentTB.Text);
             string methode_pay = methodeCBX.Text;
 
             
-            SqlConnection sc = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=F:\Folder\MiniProjet\Gestion_Association\MiniProjet\MiniProjet.mdf;Integrated Security=True;Connect Timeout=30");
-            SqlDataAdapter sda = new SqlDataAdapter("select * from MEMBRE",sc);
-            DataSet ds = new DataSet();
-            sda.Fill(ds,"membre");
-            sda = new SqlDataAdapter("select * from Payement", sc);
-            sda.Fill(ds, "payement");
-            SqlCommandBuilder cmd = new SqlCommandBuilder(sda);
-            DataRow MemberRows = ds.Tables["membre"].NewRow();
-            MemberRows["nom"] = nom;
-            MemberRows["prenom"] = prenom;
-            MemberRows["cin"] = cin;
-            MemberRows["ville"] = ville;
-            MemberRows["Code_Postal"] = code_postal;
-            MemberRows["sexe"] = sexe;
-            MemberRows["email"] = email;
-            MemberRows["num_tele"] = tele;
-            MemberRows["date_naissance"] = date_naiss;
-            MemberRows["lieu_naissance"] = lieu_naiss;
-            MemberRows["statut"] = statut;
-            MemberRows["statut_civil"] = statut_civil;
-            MemberRows["position"] = position;
-            ds.Tables["membre"].Rows.Add(MemberRows);
-            cmd.GetInsertCommand();
-            sda.Update(ds, "membre");
-            sda = new SqlDataAdapter("select TOP 1 id from MEMBRE order by id DESC ", sc);
-            sda.Fill(ds,"member_id");
-            int member_id = int.Parse(ds.Tables["member_id"].Rows[0]["id"].ToString());
 
-            /*
-            int i = ds.Tables[0].Rows.Count;
-            // .????
-            int member_id = ds.Tables[0].Rows[i - 1]["id"];
-            //*/
-            DataRow PayementRows = ds.Tables["payement"].NewRow();
-            PayementRows["membre_id"] = member_id;
-            PayementRows["methode_payement"] = methode_pay;
-            PayementRows["somme_argent"] = somme_arg;
-            ds.Tables["payement"].Rows.Add(PayementRows);
-            sda.Update(ds, "payement");
-            //sda.Update(ds);
+            //string query = "insert into MEMBRE (nom,prenom,cin,ville,Code_Postal,sexe,email,num_tele,date_naissance,lieu_naissance,statut,statut_civil,position) values('"+ nom + "','"+ prenom + "','"+ cin + "','"+ ville + "','"+ code_postal + "','"+ sexe + "','"+ email + "','"+ tele + "','"+ date_naiss + "','"+ lieu_naiss + "','"+ statut + "','"+ statut_civil + "','"+ position+"')";
+            string query = "insert into MEMBRE (nom,prenom,cin,ville,Code_Postal,sexe,email,num_tele,date_naissance,lieu_naissance,statut,statut_civil,position) values(@nom,@prenom,@cin,@ville,@code_postal,@sexe,@email,@tele,@date_naiss,@lieu_naiss,@statut,@statut_civil,@position)";
+
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=F:\Folder\MiniProjet\Gestion_Association\MiniProjet\MiniProjet.mdf;Integrated Security=True;Connect Timeout=30");
+            SqlDataAdapter sda = new SqlDataAdapter();
+
+            
+
+            sda.InsertCommand = new SqlCommand(query,con);
+            sda.InsertCommand.Parameters.Add("@nom",SqlDbType.VarChar).Value = nom;
+            sda.InsertCommand.Parameters.Add("@prenom", SqlDbType.VarChar).Value = prenom;
+            sda.InsertCommand.Parameters.Add("@cin", SqlDbType.VarChar).Value = cin;
+            sda.InsertCommand.Parameters.Add("@ville", SqlDbType.VarChar).Value = ville;
+            sda.InsertCommand.Parameters.Add("@code_postal", SqlDbType.VarChar).Value = code_postal;
+            sda.InsertCommand.Parameters.Add("@sexe", SqlDbType.Char).Value = sexe;
+            sda.InsertCommand.Parameters.Add("@email", SqlDbType.VarChar).Value = email;
+            sda.InsertCommand.Parameters.Add("@tele", SqlDbType.VarChar).Value = tele;
+            sda.InsertCommand.Parameters.Add("@date_naiss", SqlDbType.Date).Value = date_naiss;
+            sda.InsertCommand.Parameters.Add("@lieu_naiss", SqlDbType.VarChar).Value = lieu_naiss;
+            sda.InsertCommand.Parameters.Add("@statut", SqlDbType.VarChar).Value = statut;
+            sda.InsertCommand.Parameters.Add("@statut_civil", SqlDbType.VarChar).Value = statut_civil;
+            sda.InsertCommand.Parameters.Add("@position", SqlDbType.VarChar).Value = position;
+            con.Open();
+            sda.InsertCommand.ExecuteNonQuery();
+
+            DataSet ds = new DataSet();
+            sda = new SqlDataAdapter("select TOP 1 id from MEMBRE order by id DESC ", con);
+            sda.Fill(ds, "member_id");
+            int membre_id = int.Parse(ds.Tables["member_id"].Rows[0]["id"].ToString());
+
+            query = "insert into Payement (membre_id,methode_payement,somme_argent,date_payement) values (@membre_id,@methode_payement,@somme_argent,@date_payement)";
+            sda.InsertCommand = new SqlCommand(query, con);
+            sda.InsertCommand.Parameters.Add("@membre_id", SqlDbType.Int).Value = membre_id;
+            sda.InsertCommand.Parameters.Add("@methode_payement", SqlDbType.VarChar).Value = methode_pay;
+            sda.InsertCommand.Parameters.Add("@somme_argent", SqlDbType.VarChar).Value = somme_arg;
+            sda.InsertCommand.Parameters.Add("@date_payement", SqlDbType.Date).Value = ville;
+
             var mb = MessageBox.Show(this, "Membre Ajouté!", "Operation réussi", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
             if (mb == DialogResult.OK)
             {
