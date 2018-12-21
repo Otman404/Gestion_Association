106bcbaa86e213f34149f6c5024dc29881ccf237
Otman404 <outhmaneboughaba98@gmail.com>
2018-12-20 23:29:08 -0800

    Still Working

---
+++ TriggerAddDonation.sql
@@ -0,0 +1,17 @@
+﻿alter trigger trgr_add_donation on Payement
+after Insert 
+as
+begin
+
+
+
+UPDATE m
+SET m.SommeDons += i.somme_argent
+FROM Membre m
+INNER JOIN inserted i
+	ON m.id = i.membre_id
+
+insert into Donation (id_membre,id_payement,Montant)
+select i.membre_id,i.id,i.somme_argent from Inserted i
+
+end
